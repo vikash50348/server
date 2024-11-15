66089c0900b745fb156bc25e68774cb1911f5228
vikash50348 <vikashkumar50348@gmail.com>
2024-11-14 23:08:10 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("twitter");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`running on poart ${port}`);
+});
